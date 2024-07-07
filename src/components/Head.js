329fe708b432726b69cecbbe6ebb102da95f0484
James <james218.lin@gmail.com>
2024-07-07 06:56:11 -0400

    updated icon

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,7 +3,7 @@ export default function Head() {
         <head>
             <title>James Lin</title>
             <meta name="description" content="Thoughts on science, people, and technology." />
-            <link rel="icon" href="/musashi.jpg" />
+            <link rel="icon" type="image/jpeg" href="/musashi.jpg" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Newsreader:wght@400;700&display=swap" rel="stylesheet" />

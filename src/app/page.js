bdacc0e35738e4ef60a51a4e87fe4e8b99427437
James <james218.lin@gmail.com>
2024-07-07 06:38:38 -0400

    fixed bottom margin

--- src/app/page.js
+++ src/app/page.js
@@ -21,7 +21,7 @@ export default function Page() {
                     I am supported by <a href="https://www.magnificentgrants.com/">Magnificent Grants</a> and <a href="https://www.mercatus.org/emergent-ventures">Emergent Ventures</a>.
                 </p>
                 <h3 className={styles.subtitle}>This website is my digital nucleus</h3>
-                <p>
+                <p style={{marginBottom: 0}}>
                     A collection of my ever-evolving thoughts and ideas. If anything here resonates, feel free to reach out via <a href="mailto:james218.lin@gmail.com">email</a> or <a href="https://x.com/jlinbio">twitter</a>. Always excited to meet new people.
                 </p>
             </div>

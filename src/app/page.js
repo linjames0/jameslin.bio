d8f472115a655312ab01fc35d41a80f66c472db4
James <james218.lin@gmail.com>
2024-07-14 18:59:33 -0700

    added reading list

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Page() {
                 I‘m interested in synthetic biology and machine learning from an applied perspective. Come fall, I'll be developing expansion microscopy tools for brain-mapping at the <a href="https://synthneuro.org/">Boyden Lab</a>.
             </p>
             <p>
-                In my free time, I enjoy reading, climbing, sketching, and observing the world around me.
+                In my free time, I enjoy <a href="https://linjames.notion.site/bbbf004605d6430f8965e76634ba54c1?v=76c6a8574223459996028924ac5e66bd&pvs=4">reading</a>, climbing, sketching, and observing the world around me.
             </p>
             <p>
                 I'm supported by <a href="https://www.magnificentgrants.com/">Magnificent Grants</a> and <a href="https://www.mercatus.org/emergent-ventures">Emergent Ventures</a>.

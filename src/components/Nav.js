b9b003c325fb348b92f117ff1062b5921041dbb8
James <james218.lin@gmail.com>
2025-02-17 03:02:07 -0500

    post jolie

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,8 @@ import styles from "./Nav.module.css";
 export default function Nav() {
     const pathname = usePathname();
     const posts = [
+        'jolie',
+        'research',
         'young',
         'attractive',
         'focus',

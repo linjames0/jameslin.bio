d940783f6897960e9ab0c844c3a0928124eb52fa
James <james218.lin@gmail.com>
2026-01-18 23:41:11 -0500

    connecting

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,6 @@ import styles from "./Nav.module.css";
 export default function Nav() {
     const pathname = usePathname();
     const posts = [
-        'jolie',
         'research',
         'young',
         'attractive',
